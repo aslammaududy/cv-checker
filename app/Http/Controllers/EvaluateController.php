2683fb256451ee26d0afd57ea35306998c372cd1
Aslam <forjob.aslam@gmail.com>
2025-09-27 22:23:35 +0700

    set status for evaluation function and result function

--- app/Http/Controllers/EvaluateController.php
+++ app/Http/Controllers/EvaluateController.php
@@ -10,11 +10,32 @@ class EvaluateController extends Controller
     public function evaluate()
     {
         $evaluation = Evaluation::where('user_id', request()->user()->id)->firstOrFail();
+
         EvaluateCvProjectJob::dispatch($evaluation);
+
+        $evaluation->status = 'queued';
+        $evaluation->save();
 
         return response()->json([
             'id' => $evaluation->id,
             'status' => 'queued',
+        ]);
+    }
+
+    public function result()
+    {
+        $evaluation = Evaluation::where('user_id', request()->user()->id)->firstOrFail();
+
+        if ($evaluation->status == 'processing') {
+            return response()->json([
+                'id' => $evaluation->id,
+                'status' => $evaluation->status,
+            ]);
+        }
+
+        return response()->json([
+            'id' => $evaluation->id,
+            'status' => $evaluation->status,
         ]);
     }
 }
