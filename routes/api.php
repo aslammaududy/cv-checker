32a2d65fa14fca7d5fb79b80d353953dcf55dae1
Aslam <forjob.aslam@gmail.com>
2025-09-29 22:12:13 +0700

    remove MilvusController.php as it is not necessary for this project

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EvaluateController;
-use App\Http\Controllers\MilvusController;
 use App\Http\Controllers\UploadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,13 +17,3 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('/upload', UploadController::class)->middleware('auth:sanctum');
 Route::post('/evaluate', [EvaluateController::class, 'evaluate'])->middleware('auth:sanctum');
 Route::get('/result/{id}', [EvaluateController::class, 'result'])->middleware('auth:sanctum');
-
-// Milvus API routes
-Route::prefix('milvus')->group(function () {
-    Route::get('/test-connection', [MilvusController::class, 'testConnection']);
-    Route::get('/collections', [MilvusController::class, 'listCollections']);
-    Route::post('/collections', [MilvusController::class, 'createCollection']);
-    Route::delete('/collections', [MilvusController::class, 'dropCollection']);
-    Route::post('/vectors/insert-sample', [MilvusController::class, 'insertSampleVectors']);
-    Route::post('/vectors/search', [MilvusController::class, 'searchSimilar']);
-});
